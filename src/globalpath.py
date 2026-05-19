f894ed347636702184f464911f9ce935767418d8
Raffael Ho <raffyho0210@gmail.com>
2026-05-18 21:47:41 -0500

    Update globalpath.py

--- src/globalpath.py
+++ src/globalpath.py
@@ -1,1 +1,10 @@
-from PIL import Imageimport globpath = "/Users/grnho/Desktop/image-processing-fundamentals/data/plane/*"for file in glob.glob(path):    print(file)    a = Image.open(file)    rotated45 = a.rotate(45, expand = True)    rotated45.save(file+"_rotated45.png", "PNG")
+from PIL import Image
+import glob
+
+path = "/Users/grnho/Desktop/image-processing-fundamentals/data/plane/*"
+
+for file in glob.glob(path):
+    print(file)
+    a = Image.open(file)
+    rotated45 = a.rotate(45, expand = True)
+    rotated45.save(file+"_rotated45.png", "PNG")
