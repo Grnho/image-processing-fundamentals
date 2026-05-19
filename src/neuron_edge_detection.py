9d93d14cbefe6b91f51c58cc8f81e9ad425edaac
Raffael Ho <raffyho0210@gmail.com>
2026-05-18 23:28:16 -0500

    Update neuron_edge_detection.py

--- src/neuron_edge_detection.py
+++ src/neuron_edge_detection.py
@@ -1,1 +1,23 @@
-from matplotlib import pyplot as pltimport cv2import numpy as npimg = cv2.imread("../data/neuron.jpg", 0)edges = cv2.Canny(img, 100, 200)"""gradient > 200definitely edgegradient < 100definitely not edge100~200maybe edge"""plt.figure(figsize = (4,3))plt.title("Original Image")plt.imshow(img, cmap = "grey")plt.figure(figsize = (4,3))plt.title("Edge Detection")plt.imshow(edges, cmap = plt.cm.grey)
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+
+img = cv2.imread("../data/neuron.jpg", 0)
+edges = cv2.Canny(img, 100, 200)
+"""
+gradient > 200
+definitely edge
+
+gradient < 100
+definitely not edge
+
+100~200
+maybe edge
+"""
+plt.figure(figsize = (4,3))
+plt.title("Original Image")
+plt.imshow(img, cmap = "grey")
+
+plt.figure(figsize = (4,3))
+plt.title("Edge Detection")
+plt.imshow(edges, cmap = plt.cm.grey)
